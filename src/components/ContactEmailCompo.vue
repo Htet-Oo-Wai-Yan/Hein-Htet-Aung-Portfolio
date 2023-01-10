b262196c7e879f791a94a77c1f2cb68e50d84ba0
Grim Reaper <htetoowaiyan002@gmail.com>
2023-01-10 06:12:57 -1200

    portfolio-contact-email-setup-component v-1

--- src/components/ContactEmailCompo.vue
+++ src/components/ContactEmailCompo.vue
@@ -1,5 +1,58 @@
 <template>
-  
+    <div class="contact-email">
+        <div class="contact-form-main">
+            <form action="https://formspree.io/f/mayzykqq" method="POST" class="contact-form">  
+                
+                <div class="form-detail">
+                    <input type="text" id="cliname" name="cliname" placeholder="Name#" required/>
+                    <label for="cliname">Please enter your name</label>
+                </div>
+
+                <div class="form-detail">
+                    <input type="text" id="clicompany" name="clicompany" placeholder="Company Name#" required />
+                    <label for="clicompany">Please enter your company name</label>
+                </div>
+                
+                <div class="form-detail">
+                    <input type="email" id="cliemail" name="cliemail" placeholder="Email#" required/>
+                    <label for="cliemail">Please enter your email</label>
+                </div>
+
+                <div class="form-detail">
+                    <input type="number" id="clinumber" name="clinumber" placeholder="Phone Number#" required/>
+                    <label for="clinumber">Please enter your number</label>
+                </div>
+
+                <div class="form-detail">
+                    <select id="cliservice" name="cliservice" required>
+                      
+                        <option>
+                            <input type="radio" id="cliservice-content" name="cliservice-content" value="Content Writing"/>
+                            <label for="cliservice-content">Content Writing</label>
+                        </option>
+
+                        <option>
+                            <input type="radio" id="cliservice-design" name="cliservice-design" value="Graphic Design"/>
+                            <label for="cliservice-design">Graphic Design</label>
+                        </option>
+
+                        <option>
+                            <input type="radio" id="cliservice-marketing" name="cliservice-marketing" value="Digital Marketing"/>
+                            <label for="cliservice-marketing">Digital Marketing</label>
+                        </option>
+
+                    </select>
+                </div>
+
+                <div class="form-detail">
+                    <textarea id="climessage" name="climessage" rows="1" placeholder="Please let me know a bit about your project so i can help you better price and timeframe" required></textarea>
+                </div>
+
+                <button type="submit" value="submit" id="submit-btn">Submit</button>
+
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,3 +64,10 @@ export default {
 <style lang="scss" scoped>
 
 </style>
+<!-- 
+name
+companyname
+company email
+contact ph number
+interested service
+little detail about project -->
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <ContactEmailCompo></ContactEmailCompo>
   </div>
 </template>
+
+
+<script>
+// @ is an alias to /src
+import ContactEmailCompo from '@/components/ContactEmailCompo.vue'
+
+export default {
+  components: {
+    ContactEmailCompo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home{
+  background-color: #000;
+}
+</style>
